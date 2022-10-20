5388530cf3c4ad49fe994fcbc6c3d09767436408
Rehan Ahmad <rehanmakhdoomi93@gmail.com>
2022-10-20 13:51:58 +0530

    product list ui

--- src/component/Main.js
+++ src/component/Main.js
@@ -3,32 +3,41 @@ import { addToCart, emptyCart, removeToCart } from "../redux/action";
 import { useDispatch } from "react-redux";
 import { productList } from "../redux/productAction";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function Main() {
   const dispatch = useDispatch();
   const data = useSelector((state) => state.productData);
   console.log("data is main component from saga", data);
-  const product = {
-    name: "I Phone",
-    type: "mobile",
-    price: "10000",
-    color: "red",
-  };
+
+  useEffect(() => {
+    dispatch(productList());
+  }, []);
   return (
     <div>
-      <button onClick={() => dispatch(addToCart(product))}>Add To Cart</button>
-      <div>
-        <button onClick={() => dispatch(removeToCart(product.name))}>
-          Remove From Cart
-        </button>
-      </div>
       <div>
         <button onClick={() => dispatch(emptyCart())}>Empty Cart</button>
       </div>
-      <div>
-        <button onClick={() => dispatch(productList())}>
-          Get Product List
-        </button>
+
+      <div className="product-container">
+        {data.map((item, id) => (
+          <div className="product-item" key={id}>
+            <img src={item.photo} alt="" />
+            <div>name:{item.name}</div>
+            <div>Color:{item.color}</div>
+            <div>Price:{item.price}</div>
+            <div>Category:{item.category}</div>
+            <div>Brand:{item.brand}</div>
+            <div>
+              <button onClick={() => dispatch(addToCart(item))}>
+                Add To Cart
+              </button>
+              <button onClick={() => dispatch(removeToCart(item.id))}>
+                Remove To Cart
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
